431ef7ace8cb00f4f204575db26d285e6ff3c48f
strangercacaus <120672788+strangercacaus@users.noreply.github.com>
2023-04-08 17:29:42 -0300

    Update - M08 - AWS

---
+++ M08_AWS/aws.py
@@ -0,0 +1,23 @@
+import boto3
+from botocore import exceptions
+from botocore.exceptions import ClientError
+import logging
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv('/Users/caueausec/Documents/GitHub/how_bootcamps_data_engineering/.env')
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id = getenv('AWS_ID'),
+    aws_secret_access_key = getenv('AWS_KEY')
+)
+
+def criar_bucket(nome):
+    try:
+        s3_client.create_bucket(Bucket=nome)
+    except ClientError as e:
+        logging.error(e)
+    return True
+
+criar_bucket('obs-3254f-mrworlwide')
